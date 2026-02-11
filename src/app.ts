2629278f3c9247e38fca07e70acccc02ffdcfbb2
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2026-02-11 19:17:32 +0100

    Add error handler and fix npm run dev script

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
+
+import usersRoutes from "./routes/users-routes.js";
+
+import { HttpError } from "./models/http-error.js";
 
 const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route.", 404);
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(error.status ?? 500)
+    .json({ message: error.message ?? "Internal Server Error" });
+});
 
 export default app;
